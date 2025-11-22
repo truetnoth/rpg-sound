80ed5fef70897ff60a82db06ccf18101cb5b0063
Valeriy Trusevich <truetnoth@gmail.com>
2025-11-22 18:11:02 +0300

    PWA

---
+++ service-worker.js
@@ -0,0 +1,74 @@
+const CACHE_NAME = 'dnd-soundboard-v1.0.0';
+const urlsToCache = [
+  './',
+  './index.html',
+  './css/style.css',
+  './js/audio-engine.js',
+  './js/ui-controller.js',
+  './js/storage.js',
+  './js/app.js',
+  './manifest.json'
+];
+
+// Установка Service Worker
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+  self.skipWaiting();
+});
+
+// Активация Service Worker
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            console.log('Deleting old cache:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+// Обработка запросов
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        // Возвращаем из кэша, если есть
+        if (response) {
+          return response;
+        }
+        
+        // Клонируем запрос
+        const fetchRequest = event.request.clone();
+        
+        return fetch(fetchRequest).then((response) => {
+          // Проверяем валидность ответа
+          if (!response || response.status !== 200 || response.type !== 'basic') {
+            return response;
+          }
+          
+          // Клонируем ответ
+          const responseToCache = response.clone();
+          
+          // Кэшируем новый ответ
+          caches.open(CACHE_NAME)
+            .then((cache) => {
+              cache.put(event.request, responseToCache);
+            });
+          
+          return response;
+        });
+      })
+  );
+});
